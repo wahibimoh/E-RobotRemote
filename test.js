7e7cde508fbe8f5fc5d30682c79e4e189f083b19
Alwahibi <m3dteam@gmail.com>
2023-06-06 16:12:19 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,19 @@
 (function () {
 if( typeof window.res !== 'undefined') return;
 
-window.res = 1;
-for(div of $("div:contains('test_123')")) if(div.innerHTML == 'test_123') res = div
+var selectedElement = null;
+window.res = document.querySelectorAll('div'); 
+
+for(div of window.res) 
+	if(div.innerHTML == 'test_123') 
+		selectedElement = div
 
 function time() {
   var d = new Date();
   var s = d.getSeconds();
   var m = d.getMinutes();
   var h = d.getHours();
-  res.innerHTML = 
+  selectedElement.innerHTML = 
     ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
 }
 
